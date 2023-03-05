393cf487125c131bae95bf2f35b9072b45a37c61
Greg <euro@users.noreply.github.com>
2023-03-05 12:45:36 -0500

    formatting

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,6 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 
 module.exports = function (eleventyConfig) {
-
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   return {
